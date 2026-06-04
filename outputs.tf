1662fa29cd927b25781f102633eb911ace5aaaf2
pranitha1437-ux <pranitha1437@gmail.com>
2026-06-04 17:15:39 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "population_id" {
+  value = pingone_population.employees.id
+}
 
+output "saml_app_id" {
+  value = pingone_application.saml_app.id
+}
+
+output "signing_key_id" {
+  value = pingone_key.saml_signing_key.id
+}
