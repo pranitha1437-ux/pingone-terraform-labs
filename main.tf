39f564490d7a66fdfbb7fa3ef85f7ff044d80770
Pranitha <pranitha1437@gmail.com>
2026-06-04 16:59:36 +0530

    Initial Terraform PingOne project

--- main.tf
+++ main.tf
@@ -21,3 +21,31 @@ resource "pingone_population" "employees" {
   name           = "Terraform Employees"
   description    = "Created using Terraform"
 }
+resource "pingone_key" "saml_signing_key" {
+  environment_id      = var.environment_id
+  name                = "Terraform SAML Key"
+  algorithm           = "RSA"
+  key_length          = 2048
+  signature_algorithm = "SHA256withRSA"
+  subject_dn          = "CN=Terraform SAML Key,O=Example,C=SG"
+  usage_type          = "SIGNING"
+  validity_period     = 365
+}
+
+resource "pingone_application" "saml_app" {
+  environment_id = var.environment_id
+
+  name    = "Terraform SAML App"
+  enabled = true
+
+  saml_options = {
+    acs_urls           = ["https://example.com/saml/acs"]
+    assertion_duration = 3600
+    sp_entity_id       = "https://example.com/sp"
+
+    idp_signing_key = {
+      key_id    = pingone_key.saml_signing_key.id
+      algorithm = "SHA256withRSA"
+    }
+  }
+}
