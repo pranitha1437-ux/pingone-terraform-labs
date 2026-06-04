16e3209466ec34f89863429862023979624fc9ee
pranitha1437-ux <pranitha1437@gmail.com>
2026-06-04 17:13:49 +0530

    Update varibale.tf

--- varibale.tf
+++ varibale.tf
@@ -1,1 +1,15 @@
+variable "client_id" {
+  description = "PingOne Client ID"
+  type        = string
+}
 
+variable "client_secret" {
+  description = "PingOne Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "environment_id" {
+  description = "PingOne Environment ID"
+  type        = string
+}
